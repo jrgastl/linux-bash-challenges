c933497a370c9d1b9faa218074f78f1488277c19
Ricardo <jrgastl@gmail.com>
2025-08-04 23:24:21 +0200

    Update first challenge of bash scripting course

---
+++ learning-bash-scripting/other-examples.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+#Brace expansion
+echo ""
+echo "Brace Expansion Example"
+echo {test,this,text}_{1..3}
+
+#Parameter expansion and variables
+echo "" 
+echo "Parameter Expansion and Variables Example"
+declare -u a="Joao"
+echo -e "Replace this name "$a"...\n...by this name "${a/JOAO/Ricardo}""
+
+#Arithmetic expansion and evaluation
+echo ""
+echo "Arithmetic Expansion and Evaluation Examples"
+declare -i mynum=48
+echo "The value of the variable mynum is $mynum"
+(( mynum+=2 ))
+echo "Mynum incremented by 2 is $mynum"
+echo "Mynum times 3 divided by 5 is $(( (mynum * 3) / 5 ))" 
+echo "bc can help us to deal with decimals, like in 100 divided by 3 that is equal to $(echo "scale=3; 100/3" | bc)" 
+echo "RANDOM can be used to give us a random number between 1 and 10 : $((1 + RANDOM % 10))"
+
+#Extended test
+echo "" 
+echo "Extended Test Example"
+echo "Is /home a directory?"
+echo $([[ -d /home ]] && echo /home is a directory)
+echo "Is /home or /house a directory?"
+echo $([[ -d /home || -d /house ]] && echo "yes, one of them is a directory") 
+echo "Are both /home and /house directories?"
+echo $([[ -d /home && -d /house ]]; echo $?)
+
+#Arrays
+echo 
---
+++ learning-bash-scripting/system-report.sh
@@ -0,0 +1,31 @@
+# Task Description
+#Compose a script to show some system information.
+#Use some standard tools, like df, free, or others.
+#Use awk or sed to extract text from output, if you know them.
+#Use formatted text.
+#Set aside 20 minutes for this task.
+
+#!/usr/bin/env bash
+
+declare -i raw_storage_total=$(df | grep "/dev/sdd" | awk '{print $2}')
+declare -i raw_storage_used=$(df | grep "/dev/sdd" | awk '{print $3}')
+declare -i raw_storage_free=$(df | grep "/dev/sdd" | awk '{print $4}')
+declare -i raw_memory_total=$(free | grep "Mem" | awk '{print $2}')
+declare -i raw_memory_used=$(free | grep "Mem" | awk '{print $3}')
+declare -i raw_memory_free=$(free | grep "Mem" | awk '{print $4}')
+storage_total=$(echo "scale=1; $raw_storage_total / 1024 / 1024" | bc )
+storage_used=$(echo "scale=1; $raw_storage_used / 1024 / 1024" | bc )
+storage_free=$(echo "scale=1; $raw_storage_free / 1024 / 1024" | bc )
+memory_total=$(echo "scale=1; $raw_memory_total / 1024 / 1024" | bc )
+memory_used=$(echo "scale=1; $raw_memory_used / 1024 / 1024" | bc )
+memory_free=$(echo "scale=1; $raw_memory_free / 1024 / 1024" | bc )
+p_storage=$(echo "scale=1; $raw_storage_used * 100 / $raw_storage_total" | bc )
+p_memory=$(echo "scale=1; $raw_memory_used * 100 / $raw_memory_total" | bc)
+
+echo ""
+echo -e "\033[35;46m         System Information          \033[0m"
+echo -e "\033[35;46m------------------------------------ \033[0m"
+echo -e "\033[35;46m        |  Total  |  Used  |  Free  |\033[0m"
+printf "\033[35;46m%-8s|%8.1fG|%7.1fG|%7.1fG|\033[0m\n" "Storage" $storage_total $storage_used $storage_free "Memory" $memory_total $memory_used $memory_free
+echo -e "\033[35;46m-------------------------------------\033[0m\n"
+printf "\033[91;40;5mYou are using %.1f%% of the storage and %.1f%% of the  memory!\033[0m\n" $p_storage $p_memory
