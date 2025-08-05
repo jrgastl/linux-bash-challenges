a5825919dae28bd64b12e92af1bf5d93290cb231
Ricardo <jrgastl@gmail.com>
2025-08-05 21:15:01 +0200

    starting the challenge control-structures

---
+++ learning-bash-scripting/control-structures.sh
@@ -0,0 +1,4 @@
+#Task Description
+#Compose a script that uses control structure to react to input or other conditions.
+#Examples include a quote viewer, a dice roll, or a card draw.
+#Seat aside 20 minutes to build your script.
