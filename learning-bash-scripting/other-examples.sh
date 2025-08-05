f8150ebb89b8f2dfacb1e72fff818ded35288458
Ricardo <jrgastl@gmail.com>
2025-08-05 20:31:11 +0200

    other-examples.sh updated

--- learning-bash-scripting/other-examples.sh
+++ learning-bash-scripting/other-examples.sh
@@ -1,19 +1,23 @@
 #!/usr/bin/env bash
 
+yellow="\033[33m"
+normal="\033[0m"
+blue="\033[34m"
+
 #Brace expansion - an example on how to optimize a command by using the brace expansion.
 echo ""
-echo "Brace Expansion Example"
+echo -e "$yellow"Brace Expansion Example$normal""
 echo {test,this,text}_{1..3}
 
 #Parameter expansion and variables - some examples on how to deal with variables and parameters.
 echo "" 
-echo "Parameter Expansion and Variables Example"
+echo -e "$yellow"Parameter Expansion and Variables Example$normal""
 declare -u a="Joao"
 echo -e "Replace this name "$a"...\n...by this name "${a/JOAO/Ricardo}""
 
 #Arithmetic expansion and evaluation - some examples on how to do arithmetic operations.
 echo ""
-echo "Arithmetic Expansion and Evaluation Examples"
+echo -e "$yellow"Arithmetic Expansion and Evaluation Examples$normal""
 declare -i mynum=48
 echo "The value of the variable mynum is $mynum"
 (( mynum+=2 ))
@@ -24,7 +28,7 @@ echo "RANDOM can be used to give us a random number between 1 and 10 : $((1 + RA
 
 #Extended test - some examples of comparisons that can be made, including to create conditions.
 echo "" 
-echo "Extended Test Example"
+echo -e "$yellow"Extended Test Example$normal""
 echo "Is /home a directory?"
 echo $([[ -d /home ]] && echo /home is a directory)
 echo "Is /home or /house a directory?"
@@ -33,15 +37,27 @@ echo "Are both /home and /house directories?"
 echo $([[ -d /home && -d /house ]]; echo $?)
 
 #Arrays - some examples on how to do arrays
-declare -a car_brands=(ferrari porsche maseratti)
-echo ${car_brands[1]}
-car_brands[5]=lamborghini
-echo ${car_brands[5]}
-for i in {0..5};do echo $i: ${car_brands[i]}; done 
+echo ""
+echo -e "$yellow"Indexed array example$normal""
+declare -a car_brands=("ferrari" "porsche" "maseratti")
+echo -e "$blue${car_brands[1]}$normal is the second item in the array"
+car_brands[5]="lamborghini"
+car_brands+=("aston martin")
+echo -e "$blue${car_brands[5]}$normal is the sixth item in the array"
+echo -e "$blue${car_brands[@]}$normal are all items in the array"
+echo -e "$blue${car_brands[@]: -1}$normal is last item in the array"
+for i in {0..6};do echo $i: ${car_brands[i]}; done 
+echo ""
+echo -e "$yellow"Associative array example$normal""
 declare -A vm
 vm["name"]="main"
 vm["cpu"]=4
 vm["gpu"]="nvidia"
 vm["disk"]=100
+
+echo -e "$blue${vm["name"]}$normal is the value of the "name" key in the vm array"
+echo -e "$blue${vm["cpu"]}$normal is the value of the "cpu" key in the vm array"
+echo -e "$blue${vm["gpu"]}$normal is the value of the "gpu" key in the vm array"
+echo -e "$blue${vm["disk"]}$normal is the value of the "disk" key in the vm array"
 
 echo "The Virual Machine has ${vm["cpu"]} cores and a ${vm["gpu"]} gpu"
