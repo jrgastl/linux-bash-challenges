09a64dc269d52029a50221b9ee220f4b5cb96bf1
Ricardo <jrgastl@gmail.com>
2025-08-08 09:46:13 +0000

    challenge 8 in progress

--- learning-bash-scripting/slowdicepassgen.sh
+++ learning-bash-scripting/slowdicepassgen.sh
@@ -1,9 +1,15 @@
+#Author Notes:
+#For this task I tried to implement as many skills learned in the chapter as possible. Since there is a much more simple way to do the same
+#the same thing 
 #Task Description
 #Compose a script that uses control structure to react to input or other conditions.
 #Examples include a quote viewer, a dice roll, or a card draw.
 #Seat aside 20 minutes to build your script.
 #
 #My attempt: diceware passphrase generator
+yellow="\033[33m"
+normal="\033[0m"
+
 declare -a codearr
 for i in $(seq 1 $1); do
 	declare code=""
@@ -12,7 +18,7 @@ for i in $(seq 1 $1); do
 		done
 	codearr+=($code)
 	done
-echo ${codearr[@]}
+echo -e "The generated code is $yellow${codearr[@]}$normal, looking for the matching words..."
 declare -a passarr
 while read f; do
 	number=$(echo "$f" | awk '{printf $1}')
@@ -23,4 +29,4 @@ while read f; do
 		fi
 	done
 done < ./text/wordlist.asc
-echo ${passarr[@]}
+echo -e "Your pass phrase is $yellow${passarr[@]}$normal"
